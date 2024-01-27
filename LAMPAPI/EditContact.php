1e6b74398a36ba6852c24bd164fd23b8f21e09d7
ryanglatour <145807139+ryanglatour@users.noreply.github.com>
2024-01-27 18:57:22 -0500

    Add files via upload

---
+++ LAMPAPI/EditContact.php
@@ -0,0 +1,41 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+    $field = $inData["field"];
+    $edit = $inData["edit"];
+	$ID = $inData["ID"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET ? = ? WHERE ID = ?");
+		$stmt->bind_param("sss", $field, $edit, $ID);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
