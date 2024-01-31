35fe9f786f4af52456becaa3327b2e75e8d51d95
Ryan <ryanglatour@gmail.com>
2024-01-31 00:15:42 -0500

    Removed testing logs. I think it works!

--- LAMPAPI/Registration.php
+++ LAMPAPI/Registration.php
@@ -2,8 +2,6 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-	error_reporting(E_ALL); 
-	ini_set('display_errors', 1);
 
 	$inData = getRequestInfo();
 
