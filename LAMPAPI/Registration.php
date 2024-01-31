2aa7089e4c2abb66d6ac44ecd45b4e777852b525
Ryan <ryanglatour@gmail.com>
2024-01-31 00:14:31 -0500

    Searches to see if username is taken first

--- LAMPAPI/Registration.php
+++ LAMPAPI/Registration.php
@@ -19,12 +19,24 @@
 	}
 	else
 	{
+		$stmt = $conn->prepare("SELECT * FROM Users WHERE Login = ?");
+		$stmt->bind_param("s", $login);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if (mysqli_num_rows($result) > 0) {
+			http_response_code(200);
+			returnWithError("Username taken");
+		}
+		else {
 		$stmt = $conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
 		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
 		returnWithError("");
+		}
+	
 	}
 
 	function getRequestInfo()
