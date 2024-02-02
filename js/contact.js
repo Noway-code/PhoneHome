d9242f7b15dcf3c58b4d9b8672ffe94c53b0f612
Ryan <ryanglatour@gmail.com>
2024-02-02 01:38:06 -0500

    oh

--- js/contact.js
+++ js/contact.js
@@ -22,9 +22,9 @@ function fetchFirstLoadedContacts() {
                     console.log(jsonObject.error);
                     return;
                 }
-                let x = jsonObject.results[i].ID;
-                console.log("id: " + x);
+                
 				for (let i = 0; i < jsonObject.results.length; i++) {
+                    console.log("id: " + jsonObject.results[i].ID);
 					createContact(jsonObject.results[i].FirstName, jsonObject.results[i].LastName, 
                         jsonObject.results[i].Phone, jsonObject.results[i].Email, jsonObject.results[i].ID);
 				}
