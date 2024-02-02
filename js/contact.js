948e99ccc0a1bec2ca574506d503f5b2a53d5a60
Ryan <ryanglatour@gmail.com>
2024-02-02 02:02:27 -0500

    Edit

--- js/contact.js
+++ js/contact.js
@@ -113,7 +113,7 @@ function addContactHandler() {
     addContact(firstName, lastName, phoneNumber, email);
 
     // Clear table
-    $("#table tbody tr").remove();
+    $("#table_of_items tbody tr").remove();
 
     fetchFirstLoadedContacts();
     
