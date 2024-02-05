6983fcbc382b2b762794de4d2213eed6a50267b7
Camilo Alvarez-Velez <42259477+Noway-code@users.noreply.github.com>
2024-02-05 12:40:19 -0500

    Merging contact stylings (#16)
    * Added spacing between the logout and search bar. Using absolute positon disregards any other element, so removed that and opted to use styling to push the button to the right
    
    * Rounded off button edges, supposedly now fixed the background not extending with new contacts that overflow.
    
    * Made wrong assumptions about CSS, reverted background and made it repeat the background. Added html element to it so it should hopefully achieve desired effect
    
    * Forgot to change local values oops
    
    * Adjust: Directory issues and removed old stlyings
    
    * Adjust: Change padding styling, general cleanup, and rounded contacts table corners.
    
    ---------
    
    Co-authored-by: Jhon Tabio <JhonTabioCS@gmail.com>

--- js/code.js
+++ js/code.js
@@ -236,6 +236,39 @@ function addContact(fName, lName, pNumber, email)
 
 }
 
+// function searchContacts ()
+// {
+// 	let tmp = {userId: parseInt(localStorage.getItem("userId")), search: "" };
+
+// 	let jsonPayload = JSON.stringify( tmp );
+
+// 	let url = urlBase + '/SearchContacts.' + extension;
+// 	let xhr = new XMLHttpRequest();
+
+// 	xhr.open("POST", url, true);
+// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+// 	try
+// 	{
+// 		xhr.onreadystatechange = function()
+// 	 	{
+// 			if (this.readyState == 4 && this.status == 200) {
+// 				let jsonObject = JSON.parse(xhr.responseText);
+//                 if (jsonObject.error) {
+//                     console.log(jsonObject.error);
+//                     return;
+//                 }
+// 				for (let i = 0; i < jsonObject.results.length; i++) {
+
+// 				}
+// 			}
+// 		}
+// 		xhr.send(jsonPayload);
+// 	}
+// 	catch (err) {
+
+// 	}
+// }
 
 function deleteContact(fName, lName) {
 	let tmp = {firstName:fName, lastName:lName, userId:parseInt(localStorage.getItem("userId"))};
@@ -294,80 +327,3 @@ function editContact(field, edit, ID) {
 		alert("error in editing contact");
 	}
 }
-
-
-// function addColor()
-// {
-// 	let newColor = document.getElementById("colorText").value;
-// 	document.getElementById("colorAddResult").innerHTML = "";
-//
-// 	let tmp = {color:newColor,userId,userId};
-// 	let jsonPayload = JSON.stringify( tmp );
-//
-// 	let url = urlBase + '/AddColor.' + extension;
-//
-// 	let xhr = new XMLHttpRequest();
-// 	xhr.open("POST", url, true);
-// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-// 	try
-// 	{
-// 		xhr.onreadystatechange = function()
-// 		{
-// 			if (this.readyState == 4 && this.status == 200)
-// 			{
-// 				document.getElementById("colorAddResult").innerHTML = "Color has been added";
-// 			}
-// 		};
-// 		xhr.send(jsonPayload);
-// 	}
-// 	catch(err)
-// 	{
-// 		document.getElementById("colorAddResult").innerHTML = err.message;
-// 	}
-//
-// }
-//
-// function searchColor()
-// {
-// 	let srch = document.getElementById("searchText").value;
-// 	document.getElementById("colorSearchResult").innerHTML = "";
-//
-// 	let colorList = "";
-//
-// 	let tmp = {search:srch,userId:userId};
-// 	let jsonPayload = JSON.stringify( tmp );
-//
-// 	let url = urlBase + '/SearchColors.' + extension;
-//
-// 	let xhr = new XMLHttpRequest();
-// 	xhr.open("POST", url, true);
-// 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-// 	try
-// 	{
-// 		xhr.onreadystatechange = function()
-// 		{
-// 			if (this.readyState == 4 && this.status == 200)
-// 			{
-// 				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
-// 				let jsonObject = JSON.parse( xhr.responseText );
-//
-// 				for( let i=0; i<jsonObject.results.length; i++ )
-// 				{
-// 					colorList += jsonObject.results[i];
-// 					if( i < jsonObject.results.length - 1 )
-// 					{
-// 						colorList += "<br />\r\n";
-// 					}
-// 				}
-//
-// 				document.getElementsByTagName("p")[0].innerHTML = colorList;
-// 			}
-// 		};
-// 		xhr.send(jsonPayload);
-// 	}
-// 	catch(err)
-// 	{
-// 		document.getElementById("colorSearchResult").innerHTML = err.message;
-// 	}
-//
-// }
